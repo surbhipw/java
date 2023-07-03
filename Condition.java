fdc0593a82057b281b267682f77d301d4fb8c897
surbhipw <bhongadesurbhi@gmail.com>
2023-07-03 20:36:04 +0530

    first commit

---
+++ Condition.java
@@ -0,0 +1,41 @@
+ import java.util.Scanner;
+
+import javax.lang.model.element.VariableElement;
+
+class Condition{
+    public static void main(String args[]){
+         Scanner scan = new Scanner(System.in);
+
+//   First if & else 
+//         System.out.println("Enter the number");
+//         System.out.println("Enter first number");
+
+//         int a = scan.nextInt();
+//         System.out.println("Enter second number");
+//                int b = scan.nextInt();
+       
+//         if(a==b){
+// System.out.println("condition is true");
+//         }
+//         else{
+//             System.out.println("condition is false");
+//         }
+
+
+
+//Second Logical and operator
+System.out.println("Enter first Number");
+int a  = scan.nextInt();
+System.out.println("Enter second Number");
+int b  = scan.next
+Int();
+if(a>b && a<b){
+     System.out.println("condition is true");
+}else{
+    System.out.println("condition is false");
+}
+
+
+
+    }
+}
